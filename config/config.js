c9af1468fa746a1486789b5cf457ab35d30d745b
mohitpatell <mohit@wednesday.is>
2020-05-17 23:32:35 +0530

    configuration changed

--- config/config.js
+++ config/config.js
@@ -5,13 +5,13 @@ config()
 /*eslint-disable */
 module.exports = {
     MONGO_URL: process.env.MONGO_URL || '',
-    SERVER_PORT: 3000,
-    // REDIS_URL: process.env.REDIS_URL,
+    SERVER_PORT: 5000,
+    // REDIS_URL: redis://127.0.0.1:6379,
     REDIS_URL: 'redis://127.0.0.1:6379',
     LOG_LEVEL: process.env.LOG_LEVEL,
     NODE_ENV: process.env.NODE_ENV,
     FE_URL: process.env.FE_URL,
-    SENDGRID_EMAIL: process.env.SENDGRID_FROM_EMAIL,
-    SENDGRID_SENDER: process.env.SENDGRID_SENDER,
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
+    SENDGRID_EMAIL: 'mohitpatel9753@gmail.com',
+    SENDGRID_SENDER: 'Mohit',
+    SENDGRID_API_KEY: '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'
 }
