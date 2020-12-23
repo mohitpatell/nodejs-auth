8b60381f4648eb0d8c80810bf2f18092dbd37920
Mohit Patel <mohitpatel9753@gmail.com>
2020-12-23 18:41:04 +0530

    updated users file

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 
-const { genJWTToken, removeToken, validateUser,isAuthenticate } = require('../common/helpers/auth')
+const { isAuthenticate } = require('../common/helpers/auth')
 const {
-    signup, 
+    signup,
     reSendCode,
-    login, 
-    logout, 
-    verifyEmail, 
+    login,
+    logout,
+    verifyEmail,
     resetPassword
 } = require('../controllers')
 
@@ -17,21 +17,12 @@ const {
     validateResetPass
 } = require('../common/validators')
 
-
 const routes = Router()
 
 routes.post('/register', validateSignup, signup)
 routes.post('/resend', reSendCode)
 routes.post('/login', validateLogin, login)
 routes.post('/logout', isAuthenticate, logout)
-routes.post('/verify-user',isAuthenticate,async(req,res)=> {
-    // console.log(req.body);
-    const r = await validateUser(req.body.token,req.body.username);
-    res.json({
-        msg:r,
-        res:'success'
-    })
-})
 routes.post('/email/verify', validateVerifyEmail, verifyEmail)
 routes.post('/password/reset', validateResetPass, resetPassword)
 
